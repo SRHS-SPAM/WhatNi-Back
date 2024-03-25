3dae1ec5b1c8929d8f6e5451e3fc3aa3c1b808e4
정삼복 <68502584+3boku@users.noreply.github.com>
2024-03-25 19:52:43 +0900

    jwt

--- contorllers/root.go
+++ contorllers/root.go
@@ -4,6 +4,7 @@ import (
 	"WhatNi-Back/repositories"
 	"WhatNi-Back/services"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func NewContorllers(port string) {
@@ -22,6 +23,21 @@ func NewContorllers(port string) {
 	})
 	r.PUT("/wh/:hakbun", func(c *gin.Context) {
 		services.Wh(c, db)
+	})
+	r.GET("/generate-token", func(c *gin.Context) {
+		username, exists := c.GetQuery("username")
+		if !exists {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Username query parameter is required"})
+			return
+		}
+
+		token, err := services.GenerateJWT(username)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate JWT token"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"token": token})
 	})
 
 	err := r.Run(port)
