75824ad947d4d35729d2719d70d10e4df5830084
정삼복 <68502584+3boku@users.noreply.github.com>
2024-03-26 11:45:56 +0900

    사유적는 칸

---
+++ entities/reason.go
@@ -0,0 +1,5 @@
+package entities
+
+type Reason struct {
+	Reason string `json:"reason"`
+}
---
+++ services/late-wh.go
@@ -0,0 +1,10 @@
+package services
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func Reason(c *gin.Context, db *gorm.DB) {
+
+}
